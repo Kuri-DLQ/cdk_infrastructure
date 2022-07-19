840e46e8ecb8459975be55c9c21a1c025f4c2fbe
Anna Kolliakou <akolliakou@gmail.com>
2022-07-19 10:51:59 +0100

    Second formatting completed
    Co-authored-by: Kris Ingva <krisingva@gmail.com>
    Co-authored-by: Arjun Rasodha <arjunrasodha10@gmail.com>
    Co-authored-by: Tony Beere <tonybeere87@gmail.com>

--- lambdas/postToSlackLambda.js
+++ lambdas/postToSlackLambda.js
@@ -7,18 +7,46 @@ const POST_OPTIONS = {
   method: 'POST',
 };
 
+const getDayMonthYear = (date) => {
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1;
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds();
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
 exports.handler = (event, context) => {
   for (const record of event.Records) {
-    console.log(record);
     const message = {
-      text: `A new message has arrived in "${process.env.DLQ_NAME}" dead letter queue 
-
-assigned to "${process.env.QUEUE_NAME}" main queue
-
-at ${record.Sns.Timestamp} with the following details:
-
-Message Body: ${record.Sns.Message}
-Message Attributes: ${JSON.stringify(record.Sns.MessageAttributes)}`
+      text: 'A message has failed to be processed',
+      attachments: [{
+        color: "#8697db",
+        fields:[
+          {
+            title: 'Main queue name',
+            value: `${process.env.QUEUE_NAME}`,
+          },
+          {
+            title: 'Dead letter queue name',
+            value: `${process.env.DLQ_NAME}`
+          },
+          {
+            title: 'Timestamp',
+            value: getDayMonthYear(new Date(record.Sns.Timestamp)),
+          },
+          {
+            title: 'Message',
+            value: `${record.Sns.Message}`,
+          },
+          {
+            title: 'Message Attributes',
+            value: JSON.stringify(record.Sns.MessageAttributes),
+          }
+        ]
+      }]
     };
 
 
